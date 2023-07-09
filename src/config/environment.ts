6c6bad6036773919c8230c04922229a8cc915fa0
Esteban G <tapitarias@gmail.com>
2023-07-09 14:21:05 -0500

    default db name

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,5 +2,5 @@ export const config = {
   PORT: process.env.PORT || 8080,
   DB_USER: process.env.DB_USER || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
-  DB_NAME: process.env.DB_NAME || "postgres_test",
+  DB_NAME: process.env.DB_NAME || "corserva",
 };
